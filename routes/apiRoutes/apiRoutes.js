347af54b9a2554442f7583251f4f8bdfb92d6b42
Aubree Alexander <aubree.alexander@gmail.com>
2022-05-13 18:22:11 -0500

    add delete option

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -31,7 +31,19 @@ router.post('/notes', (req, res) => {
 //delete notes
 //whenever you see :id - think req.params.id
 router.delete('/notes/:id', (req, res) => {
-    
-})
+    //create for loop that iterates over all notes and if it's not the id of the one we want to remove, we push it to a new array. 
+    //set new array equeal to database so it becomes the new database. rewrite new updated database
+    let keep = []
+    for (var i = 0; i < database.length; i++) {
+        if (database[i].id != req.params.id) {
+            keep.push(database[i])
+        }
+    }
+    database = keep;
+    fs.writeFileSync('./db/db.json', JSON.stringify(database))
+
+    //send response
+    res.json(database)
+});
 
 module.exports = router;
